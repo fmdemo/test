68dc01a3834e3bb97ff9351ff894e422b9631328
fmdemo <34957107+fmdemo@users.noreply.github.com>
2017-12-30 17:45:47 +0900

    Create led_flash_gpio22.py

---
+++ led_flash_gpio22.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 必要なライブラリの読み込み
+import RPi.GPIO as GPIO
+import time
+
+# GPIOを使うためのおまじない
+GPIO.setmode(GPIO.BCM)		# PIN番号でなく、GPIOの番号で指定
+pin=22				# GPIO 22 (15番品)を使う
+GPIO.setup(pin, GPIO.OUT)	# 出力に使用する
+
+while True:			# 以下の処理を繰り返し行う
+	GPIO.output(pin,1)	# HIGH (1) = 3.3V
+	time.sleep(1)		# 1秒待つ
+	GPIO.output(pin,0)	# LOW (0) = 0V
+	time.sleep(1)		# 1秒待つ
