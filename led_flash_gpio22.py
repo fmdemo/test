203b9d82eeb0ae9f6184aee2a1d9291463398d1f
fmdemo <34957107+fmdemo@users.noreply.github.com>
2017-12-30 19:10:12 +0900

    Update led_flash_gpio22.py

--- led_flash_gpio22.py
+++ led_flash_gpio22.py
@@ -11,6 +11,6 @@ GPIO.setup(pin, GPIO.OUT)	# 出力に使用する
 
 while True:			# 以下の処理を繰り返し行う
 	GPIO.output(pin,1)	# HIGH (1) = 3.3V
-	time.sleep(1)		# 1秒待つ
+	time.sleep(0.2)		# 0.2秒待つ
 	GPIO.output(pin,0)	# LOW (0) = 0V
-	time.sleep(1)		# 1秒待つ
+	time.sleep(0.2)		# 0.2秒待つ
